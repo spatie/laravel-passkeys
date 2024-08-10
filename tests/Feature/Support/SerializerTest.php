97ce2b6b6b1a343785726a0e8f5157072d2e13a3
freekmurze <freekmurze@users.noreply.github.com>
2024-08-10 17:57:57 +0000

    Fix styling

--- tests/Feature/Support/SerializerTest.php
+++ tests/Feature/Support/SerializerTest.php
@@ -2,6 +2,6 @@
 
 use Spatie\LaravelPasskeys\Support\Serializer;
 
-it('can serialize to json', function() {
-   Serializer::make();
+it('can serialize to json', function () {
+    Serializer::make();
 });
