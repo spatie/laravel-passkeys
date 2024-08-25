8d61cc318311d68d448f2967701360cbf40ca9e2
Freek Van der Herten <freek@spatie.be>
2024-08-25 18:58:53 +0200

    wip

--- resources/views/livewire/authenticatePasskeysScript.blade.php
+++ resources/views/livewire/authenticatePasskeysScript.blade.php
@@ -29,4 +29,10 @@
     <form id="passkey-login-form" method="POST" action="{{ route('passkeys.login') }}">
         @csrf
     </form>
+
+    @if($message = session()->get('authenticatePasskey::message'))
+        <div>
+            {{ $message }}
+        </div>
+    @endif
 </div>
