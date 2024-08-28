7cc78c567228f5152f17d9a0da66dd16ab3f62e7
Freek Van der Herten <freek@spatie.be>
2024-08-28 16:51:28 -0500

    add test

---
+++ tests/Feature/Livewire/PasskeysComponentTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use Livewire\Livewire;
+use Spatie\LaravelPasskeys\Livewire\PasskeysComponent;
+use \Spatie\LaravelPasskeys\Tests\TestSupport\Models\User;
+
+beforeEach(function() {
+   $user = User::factory()->create();
+
+   auth()->login($user);
+});
+
+it('can mount the PasskeysComponent', function () {
+    Livewire::test(PasskeysComponent::class)
+        ->assertStatus(200);
+});
