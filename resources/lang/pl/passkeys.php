f5eededa5122d3965ad8a5ef67283f830ee7c50a
Bartłomiej Gajda <b.gajda@webard.me>
2026-02-24 15:41:10 +0100

    pl lang

---
+++ resources/lang/pl/passkeys.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'authenticate_using_passkey' => 'Uwierzytelnij za pomocą klucza dostępu',
+    'create' => 'Utwórz',
+    'delete' => 'Usuń',
+    'error_something_went_wrong_generating_the_passkey' => 'Wystąpił błąd podczas generowania klucza dostępu.',
+    'invalid' => 'Nie udało się zalogować przy użyciu podanego klucza dostępu',
+    'last_used' => 'Ostatnio używany',
+    'name' => 'Nazwa',
+    'name_placeholder' => 'Wprowadź nazwę klucza dostępu',
+    'no_passkeys_registered' => 'Brak zarejestrowanych kluczy dostępu',
+    'not_used_yet' => 'Jeszcze nie używany',
+    'passkeys' => 'Klucze dostępu',
+];
